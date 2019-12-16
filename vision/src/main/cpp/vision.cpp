056da34f88a2ff2f9a7b773c5bc3c42df5decbb8
CJBuchel <connorbuchel@gmail.com>
2019-12-16 15:12:36 +0800

    Added the vision.gradle file from wml

--- vision/src/main/cpp/vision.cpp
+++ vision/src/main/cpp/vision.cpp
@@ -13,18 +13,10 @@ float height_goal;
 cv::Mat Image; // Origin Image
 cv::Mat TrackingImage; // Imaged After it has been procesed
 
+void func(int *x) {
+	*x = *x + 500;
+}
+
 void curtin_frc_vision::run() {
-	Image = vision.SetupVision(1, 60, ResHeight, ResWidth, 30, "Testing Cam", true);
-
-	while (true) {
-		if (vision.Camera.cam.sink.GrabFrame(Image) !=0) {
-			TrackingImage = vision.RetroTrack(Image, 2, 2);
-
-			cv::imshow("Origin Image", Image);
-			cv::imshow("Green Track Image", TrackingImage);
-		} else{
-			std::cout << "Error Getting Image" << std::endl;
-		}
-		cv::waitKey(30);
-	}
+	
 }
