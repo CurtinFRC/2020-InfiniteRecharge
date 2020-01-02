ce0b14e2a573d49a5aefeadfe074e391a1123b29
CJBuchel <connorbuchel@gmail.com>
2020-01-02 22:39:48 +0800

    fixed google test issues. Turns out sparks are old... who knew

--- 4788/src/main/cpp/Robot.cpp
+++ 4788/src/main/cpp/Robot.cpp
@@ -6,61 +6,82 @@ using hand = frc::XboxController::JoystickHand;
 
 double lastTimestamp;
 
+// Variables to store our wanted speed for each side
 double leftSpeed;
 double rightSpeed;
 
-wml::TalonSrx *left1;
-wml::VictorSpx *left2;
-wml::TalonSrx *right1;
-wml::VictorSpx *right2;
-
-
+double dt; //dt stands for delta time
 
 void Robot::RobotInit() {
+
+  dt = frc::Timer::GetFPGATimestamp() - lastTimestamp;
+  lastTimestamp = frc::Timer::GetFPGATimestamp();
+  // use the provided timers to calculate the time since the last cycle was run
+
+  // controllers
   xbox = new frc::XboxController(0);
+  
+  // Drivebase
+  leftMotor[0] = new frc::Spark(0);
+  rightMotor[0] = new frc::Spark(1);
 
-  left1 = new wml::TalonSrx(3);
-  left2 = new wml::VictorSpx(4);
-  right1 = new wml::TalonSrx(1);
-  right2 = new wml::VictorSpx(2);
-
-
-  left1->SetInverted(true);
-  left2->SetInverted(true);
+  leftMotor[0]->SetInverted(true);
+  rightMotor[0]->SetInverted(false);
 
 
+  // Hammer
+  hammer[0] = new frc::Spark(2);
 
-  // leftMotor[0] = new frc::Spark(0);
-  // leftMotor[0]->SetInverted(true);
-
-  // rightMotor[0] = new frc::Spark(1);
-  // rightMotor[0]->SetInverted(false);
 }
 
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic() {
+  if (pressureSensor.GetScaled() < 80) {                            // If the pressure drops below 80 psi...
+    compressor.SetTarget(wml::actuators::BinaryActuatorState::kForward); // ... turn the compressor on.
+  }
+}
 
-void Robot::DisabledInit() {}
+void Robot::DisabledInit() {
+  InterruptAll(true);
+}
 
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {
-  // leftMotor[0]->Set(xbox->kLeftYAxis);
-  // rightMotor[0]->Set(xbox->kRightYAxis);
+
+
+  // Drivebase
+  leftSpeed = xbox->GetY(hand::kLeftHand);
+  leftSpeed = xbox->GetY(hand::kRightHand);
+
+  leftMotor[0]->Set(leftSpeed);
+  rightMotor[0]->Set(rightSpeed);
+
+  // Hammer
   if (xbox->GetAButton()) {
-    leftSpeed = xbox->GetY(hand::kLeftHand);
-    rightSpeed = xbox->GetY(hand::kRightHand);
+    hammer[0]->Set(0.5);
+  } else if (xbox->GetBButton()) {
+    hammer[0]->Set(-0.5);
   } else {
-    leftSpeed = 0;
-    rightSpeed = 0;
+    hammer[0]->Set(0);
   }
 
-  left1->Set(leftSpeed);
-  left2->Set(leftSpeed);
+  // Pneumatics
+  if (xbox->GetXButton()) {
+    solenoid.SetTarget(wml::actuators::BinaryActuatorState::kForward);
+  } else {
+    solenoid.SetTarget(wml::actuators::BinaryActuatorState::kReverse); // if reverse doesn't work use forward. Might be toggle
+  }
 
-  right1->Set(rightSpeed);
-  right2->Set(rightSpeed);
+  // The update methods need to be called every cycle for the actuators to actualy do anything
+  compressor.Update(dt);
+  solenoid.Update(dt);
+
+  // Stop the solenoid if it's finished
+  if (solenoid.IsDone()) solenoid.Stop();
+
+  NTProvider::Update();     // Updates values sent to NetworkTables
 } 
 
 void Robot::TestInit() {}
--- 4788/src/main/include/Robot.h
+++ 4788/src/main/include/Robot.h
@@ -49,12 +49,25 @@ class Robot : public frc::TimedRobot, protected wml::StrategyController, protect
   void TestInit() override;
   void TestPeriodic() override;
 
-
-  // ----------Pneumatics--------------
-  // Compressor
-  wml::actuators::Compressor *compressor;
+  // controllers
   frc::XboxController *xbox;
 
+  // Drivebase
   frc::Spark *leftMotor[1];
   frc::Spark *rightMotor[1];
+
+  // Hammer
+  frc::Spark *hammer[1];
+
+  // Pneumatics. For a reason i don't know yet
+
+  // Construct a compressor instance plugged into the default PCM
+  wml::actuators::Compressor compressor{};
+
+  // Construct a (double-actuating) solenoid instance plugged into ports 0 and 1 on the default PCM, which takes roughly 0.2 secs to actuate.
+  wml::actuators::DoubleSolenoid solenoid{ 0, 1, 0.2 };
+
+  // Construct an analogue pressureSensor instance plugged into analogue port 0
+  wml::sensors::PressureSensor pressureSensor{ 0 };
+
 };
