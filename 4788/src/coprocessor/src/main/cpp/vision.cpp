9c1c5f616a46b2b1169163b7ae88c81659e217c4
CJBuchel <connorbuchel@gmail.com>
2020-01-12 02:45:41 +0800

    Added working vision tracking (Still need to fix contour size in CJ-Vision & Network Tables. But it works)

--- 4788/src/coprocessor/src/main/cpp/vision.cpp
+++ 4788/src/coprocessor/src/main/cpp/vision.cpp
@@ -12,6 +12,8 @@ cv::Mat ProcessingOutput;
 
 void curtin_frc_vision::run() {
 
+
+
 	auto inst = nt::NetworkTableInstance::GetDefault();
 	auto visionTable = inst.GetTable("VisionTracking");
 	auto table = visionTable->GetSubTable("Target");
@@ -21,9 +23,14 @@ void curtin_frc_vision::run() {
 	ImageHeight = table->GetEntry("ImageHeight");
 	ImageWidth = table->GetEntry("ImageWidth");
 
-	vision.SetupVision(&Image, 0, 30, ResHeight, ResWidth, 100, "TestCam", true);
-	vision.CustomTrack(&TrackingImage, &Image, 30, 70, 50, 255, 100, 0, 0);
+	vision.SetupVision(&Image, 4, 30, ResHeight, ResWidth, 1, "Turret Cam", true);
+	vision.CustomTrack(&TrackingImage, &Image, 30, 70, 50, 255, 1, 0, 0);
 	vision.Processing.visionHullGeneration.BoundingBox(&TrackingImage, &ProcessingOutput, &cx, &cy, 10);
+	#ifdef __DESKTOP__ 
+	std::cout << "Exposure Might be dissabled on local machine" << std::endl;
+	#else
+	system("v4l2-ctl -d /dev/video4 --set-ctrl=exposure_absolute=1");
+	#endif
 	while (true) {
 		if (vision.Camera.cam.sink.GrabFrame(Image) != 0) {
 
@@ -33,19 +40,21 @@ void curtin_frc_vision::run() {
 			vision.Output.Display("Green Filtered Image", &TrackingImage);
 			vision.Output.Display("Contour Detection", &ProcessingOutput);
 			#else 
-			vision.Camera.cam.output.PutFrame(ProcessingOutput);
+			vision.Camera.cam.output.PutFrame(Image); // Might get rid of this to speed up loop.
 			#endif
 
 			//Calc offset
 			offsetX = cx-(ResWidth/2);
 			offsetY = cy-(ResHeight/2);
 
-
+			
 			// Sending Values to nt
 			TargetX.SetDouble(offsetX);
 			TargetY.SetDouble(offsetY);
 			ImageHeight.SetDouble(ResHeight);
 			ImageWidth.SetDouble(ResWidth);
+		} else {
+			visionTable->PutBoolean("Vision Active", false);
 		}
 	}
 }
