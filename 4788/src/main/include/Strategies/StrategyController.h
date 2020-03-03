06ede66142a108463946dc02a90b4f0fc09754eb
CJ <connorbuchel@gmail.com>
2020-03-04 01:01:40 +0800

    Added Strategy Controller/builder

--- 4788/src/main/include/Strategies/StrategyController.h
+++ 4788/src/main/include/Strategies/StrategyController.h
@@ -1,22 +1,46 @@
 #pragma once
 
+#include "RobotMap.h"
 #include "strategy/Strategy.h"
 #include "TurretStrategy.h"
 #include "MagLoaderStrategy.h"
 #include "IntakeStrategy.h"
 #include "ClimberStrategy.h"
 
-class StrategyCaller : wml::StrategyBuilder {
+class ManualStrategyController : wml::StrategyBuilder {
  public:
+  ManualStrategyController(
+    const TurretManualStrategy &turret,
+    const MagLoaderManualStrategy &magLoader,
+    const ClimberManualStrategy &climber,
+    const IntakeManualStrategy &intake,
+    const RobotMap &robotMap,
+    const wml::controlers::SmartControllerGroup &controllers
+  ) : wml::Strategy("Manual"), 
+    _turret(turret), _magLoader(magLoader), _climber(climber), _intake(intake), _robotMap(robotMap), _controllers(controllers) {}
+
 
   void OnUpdate(double dt) override {
-
+    StrategyBuilder builder;
+    builder.Start();
+    builder.Add(std::make_shared<TurretManualStrategy>(_turret, _controllers));
+    builder.Add(std::make_shared<MagLoaderManualStrategy>(_magLoader, _controllers));
+    builder.Add(std::make_shared<ClimberManualStrategy>(_climber, _controllers));
+    builder.Add(std::Make_shared<IntakeManualStrategy>(_intake, _controllers));
+    Schedule(builder.Build());
   }
-
 
  private:
   Turret &_turret;
   MagLoader &_magLoader;
+  Climber &_climber;
   Intake &_intake;
+  RobotMap &_robotMap;
+  wml::controllers::SmartControllerGroup &_controllers;
 };
+
+
+class AutoStrategyController : wml::StrategyBuilder {
+
+}
 
